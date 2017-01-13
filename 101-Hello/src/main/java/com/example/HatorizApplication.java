57ffc0abeecdf11e8233a8cad4a0796c0c8fbf1a
hatoriz <ninja.hatoriz@gmail.com>
2017-01-13 22:55:55 +0700

    [JAVA] - Init hello world project and travis

---
+++ 101-Hello/src/main/java/com/example/HatorizApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HatorizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HatorizApplication.class, args);
+	}
+}
