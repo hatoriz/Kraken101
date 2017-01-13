9022c4d6b6ac0c12ff169cd811284d4333998043
hatoriz <ninja.hatoriz@gmail.com>
2017-01-13 23:36:17 +0700

    [JAVA] - first build hello world

--- 101-Hello/src/main/java/com/example/HatorizApplication.java
+++ 101-Hello/src/main/java/com/example/HatorizApplication.java
@@ -2,6 +2,8 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class HatorizApplication {
@@ -9,4 +11,32 @@ public class HatorizApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HatorizApplication.class, args);
 	}
+
+    ///------------------------------------------------------------
+    @RestController
+    class GreetingController{
+        @RequestMapping("/")
+        Greet greet(){
+            return new Greet("Hello World!");
+        }
+    }
+    class Greet {
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        private String message;
+
+        public Greet() {}
+
+        public Greet(String message) {
+            this.message = message;
+        }
+
+    }
+
 }
