d254815d6022589b59ff8af4e7c5ddf6cef9afcc
hatoriz <ninja.hatoriz@gmail.com>
2017-01-14 22:16:19 +0700

    [JAVA] - add Cross Origin annotation

--- 101-Hello/src/main/java/com/example/HatorizApplication.java
+++ 101-Hello/src/main/java/com/example/HatorizApplication.java
@@ -4,17 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.oauth2.*;
 
 // simple auth
 //import org.springframework.security.config.annotation.method.configuration.*;
 // oauth
-import org.springframework.security.*;
 
 // Simple auth
 //@EnableGlobalMethodSecurity
@@ -37,6 +35,7 @@ public class HatorizApplication {
     ///------------------------------------------------------------
     @RestController
     class GreetingController{
+        @CrossOrigin
         @RequestMapping("/")
         String Hello(){
             //return "Hello from " + env.getProperty("bootrest.customerproperty");
