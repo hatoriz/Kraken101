84ce48208228353a3573b7a2a9227e92aa848e2d
hatoriz <ninja.hatoriz@gmail.com>
2017-01-14 15:18:54 +0700

    [JAVA] - replace simple auth with oauth2

--- 101-Hello/src/main/java/com/example/HatorizApplication.java
+++ 101-Hello/src/main/java/com/example/HatorizApplication.java
@@ -5,11 +5,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.security.config.annotation.method.configuration.*;
+import org.springframework.security.oauth2.*;
 
-@EnableGlobalMethodSecurity
+// simple auth
+//import org.springframework.security.config.annotation.method.configuration.*;
+// oauth
+import org.springframework.security.*;
+
+// Simple auth
+//@EnableGlobalMethodSecurity
+
+// Oauth
+@EnableResourceServer
+@EnableAuthorizationServer
+
+// Auto spring boot app
 @SpringBootApplication
 public class HatorizApplication {
 
@@ -25,7 +39,8 @@ public class HatorizApplication {
     class GreetingController{
         @RequestMapping("/")
         String Hello(){
-            return "Hello from " + env.getProperty("bootrest.customerproperty");
+            //return "Hello from " + env.getProperty("bootrest.customerproperty");
+            return "hello";
         }
 
 //        Greet greet(){
--- 101-Hello/src/test/java/com/example/HatorizApplicationTests.java
+++ 101-Hello/src/test/java/com/example/HatorizApplicationTests.java
@@ -1,11 +1,14 @@
 package com.example;
 
+import com.sun.org.apache.regexp.internal.RE;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.*;
 import org.springframework.util.*;
+import org.springframework.security.oauth2.client.token.grant.password.*;
+import org.springframework.security.oauth2.client.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -36,4 +39,22 @@ public class HatorizApplicationTests {
 //		Assert.assertEquals("Hello World!", response.getBody().getMessage());
 //	}
 
+    @Test
+    public void testOAuthService() {
+        ResourceOwnerPasswordResourceDetails resource = new ResourceOwnerPasswordResourceDetails();
+        resource.setUsername("guest");
+        resource.setPassword("guest123");
+        resource.setAccessTokenUri("http://localhost:8081/oauth/token");
+        resource.setClientId("trustedclient");
+        resource.setClientSecret("trustedclient123");
+        resource.setGrantType("password");
+
+        DefaultOAuth2ClientContext clientContext = new DefaultOAuth2ClientContext();
+        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(resource, clientContext);
+
+        //Greet greet = restTemplate.getForObject("http://localhost:8080", Greet.class);
+
+        String result = restTemplate.getForObject("http://localhost:8081", String.class);
+        Assert.isTrue(result.equals("hello"));
+    }
 }
