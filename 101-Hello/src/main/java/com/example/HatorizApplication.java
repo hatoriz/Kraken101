3c504162753cf0e021b8652c5bb8db8f2cf8e62e
hatoriz <ninja.hatoriz@gmail.com>
2017-01-14 11:42:16 +0700

    [SECURITY] - add method-level security

--- 101-Hello/src/main/java/com/example/HatorizApplication.java
+++ 101-Hello/src/main/java/com/example/HatorizApplication.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.config.annotation.method.configuration.*;
 
+@EnableGlobalMethodSecurity
 @SpringBootApplication
 public class HatorizApplication {
 
