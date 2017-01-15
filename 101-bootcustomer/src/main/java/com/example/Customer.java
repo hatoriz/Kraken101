c69935c3efe88d88f5e411b479e267928911b008
hatoriz <ninja.hatoriz@gmail.com>
2017-01-15 11:10:03 +0700

    [JAVA] - Introduce bootcustomer

---
+++ 101-bootcustomer/src/main/java/com/example/Customer.java
@@ -0,0 +1,70 @@
+package com.example;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Optional;
+
+/**
+ * Created by hatoriz on 1/15/17.
+ */
+@RepositoryRestResource
+@Lazy
+interface CustomerRepository extends JpaRepository <Customer,Long>{
+    Optional<Customer> findByName(@Param("name") String name);
+}
+
+@Entity
+class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+
+    public Customer() {
+    }
+
+
+    public Customer(String name, String email) {
+        super();
+        this.name = name;
+        this.email = email;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer [id=" + id + ", name=" + name + ", email=" + email + "]";
+    }
+}
