29ead7b20029ee290638e97b9654103743d230a6
hatoriz <ninja.hatoriz@gmail.com>
2018-01-06 17:50:46 +0700

    Init 102 folder for REST API

---
+++ 102-hello/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+    @RestController
+    class GreetingController {
+        @CrossOrigin
+        @RequestMapping("/")
+        String Hello() {
+            return "Hello this is hatoriz";
+        }
+    }
+
+}
